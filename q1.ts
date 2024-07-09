6b690541363e425cb090545e64e2f134972b6e7c
phuriphatYoosuwan <phuriphat_yoosuwan@cmu.ac.th>
2024-07-09 16:03:31 +0700

    eiei

--- q1.ts
+++ q1.ts
@@ -3,7 +3,14 @@ import axios from "axios";
 
 /* assign interface/type to the function definition properly */
 const getUser = async (userId: number) => {
-  /* Your code here */
+  try {
+    const res_user = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    const user = res_user.data;
+    return `${user.name}`;
+  }
+  catch (err) {
+    return "INVALID USER ID";
+  }
 };
 
 //test case
@@ -16,3 +23,9 @@ getUser(input2).then((result) => console.log(result));
 
 // module.exports = getUser;
 export default getUser;
+
+/*
+รหัส นศ.: 660610784
+
+ชื่อ-สกุล : ภูริภัฏ อยู่สุวรรณ
+*/
--- q2.ts
+++ q2.ts
@@ -1,9 +1,24 @@
 // const axios = require("axios");
 import axios from "axios";
+import { getLineAndCharacterOfPosition } from "typescript";
 
 /* assign interface/type to the function definition properly */
 const getTodo = async (todoId: number) => {
-  /* Your code here */
+  try {
+    const res_todo = await axios.get(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+    const todo_data = res_todo.data; //todo data
+    const user_id = todo_data.userId; //id
+    const res_user = await axios.get(`https://jsonplaceholder.typicode.com/users/${user_id}`);
+    const user = res_user.data;
+    return {
+      owner: user.name,
+      title: todo_data.title,
+      completed: todo_data.completed
+    }
+  }
+  catch (error) {
+    return "INVALID TODO ID";
+  }
 };
 
 //test case
@@ -17,3 +32,9 @@ getTodo(input2).then((result) => console.log(result));
 getTodo(input3).then((result) => console.log(result));
 
 export default getTodo;
+
+/*
+รหัส นศ.: 660610784
+
+ชื่อ-สกุล : ภูริภัฏ อยู่สุวรรณ
+*/
